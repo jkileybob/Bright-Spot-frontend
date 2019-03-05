a1708bcbfa264677f0c676d2d9d633b345044b1e
github username <github email address>
2019-03-04 23:29:53 -0500

    about page

--- bright-spot-frontend/src/About.js
+++ bright-spot-frontend/src/About.js
@@ -2,7 +2,34 @@ import React from 'react'
 
 const About = (props) => {
   return(
-    <div> About </div>
+    <React.Fragment>
+    <div class='about'>
+      <h2>About</h2>
+
+        <div class="row">
+          <img src="https://media1.tenor.com/images/5d901873d0cae5cb691edfe4f4d81260/tenor.gif?itemid=5341262" />
+          <img src='http://66.media.tumblr.com/b46000099fb8fb2e4f5a65973f7f7886/tumblr_mnz391yH3w1qbahrjo1_250.gif' />
+          <img src="https://media1.tenor.com/images/5d901873d0cae5cb691edfe4f4d81260/tenor.gif?itemid=5341262" />
+        </div>
+
+        <p class='about-details'>You ever need a moment to yourself?</p>
+        <p class='about-details'>Has the work day got you feeling down and out?</p>
+        <p class='about-details'>Wouldn't you like a quick walk to a nearby location of your choosing that might just brighten your day?</p>
+        <h3 class='about-details'>Welcome to Bright Spot!</h3>
+        <p class='about-details'>Connecting you to small, yet bright points of happiness that exist in your area. Click on the map to find bright spots around your current location, or scroll thorugh spots to see photos.</p>
+        <h3 class='about-details'>Don't get stressed.</h3>
+        <h3 class='about-details'>Don't get sad.</h3>
+        <h3 class='about-details'>Just get out and find your</h3>
+        <h1>BrightSpot!</h1>
+
+        <div class="row">
+          <img src="https://media1.tenor.com/images/5d901873d0cae5cb691edfe4f4d81260/tenor.gif?itemid=5341262" />
+          <img src='http://66.media.tumblr.com/b46000099fb8fb2e4f5a65973f7f7886/tumblr_mnz391yH3w1qbahrjo1_250.gif' />
+          <img src="https://media1.tenor.com/images/5d901873d0cae5cb691edfe4f4d81260/tenor.gif?itemid=5341262" />
+        </div>
+        
+    </div>
+    </React.Fragment>
   )
 }
 
