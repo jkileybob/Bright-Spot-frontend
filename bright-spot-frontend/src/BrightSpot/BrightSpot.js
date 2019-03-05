740b6b206effe80e68b34bb31a76426afdae0277
github username <github email address>
2019-03-05 14:17:40 -0500

    styled view

--- bright-spot-frontend/src/BrightSpot/BrightSpot.js
+++ bright-spot-frontend/src/BrightSpot/BrightSpot.js
@@ -8,17 +8,14 @@ console.log(props)
   return(
     <div className="bright-spot">
       <h1 key={`bright-spot-name-header-${props.spot.id}`}>{props.spot.name}</h1>
-      <h3 key={`bright-spot-description-${props.spot.id}`}>{props.spot.description}</h3>
-        <img src={`${props.spot.posts[0].photo}`}/>
-
       <Link to="/bright-spots">go back.</Link>
       <Link to="/map">see it on the map.</Link>
+      <h3 key={`bright-spot-description-${props.spot.id}`}>{props.spot.description}</h3>
+      <img src={`${props.spot.posts[0].photo}`}/>
+
 
     </div>
   )
 }
 
 export default BrightSpot
-
-//need to pull img form object:
-// <img src={   props.spot.posts[0].photo
